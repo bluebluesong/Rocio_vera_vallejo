e46706b0b618f78244d411d1895103e943c748b1
Rocío_Vera_Vallejo <butterfly.zaphire@gmail.com>
2024-03-20 21:23:42 -0500

    Hito2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ document.getElementById("finalizar").onclick=function(){
   alert("Hola")
 }
 
+document.getElementById("comenzar").onclick=function(){
+ document.getElementById("root").style.display="block";
+ 
+ document.getElementById("bienvenida").style.display="none";
+ document.getElementById("velo").style.display="none";
+
+ var nombre=document.getElementById("nombre").value;
+ document.getElementById("usuario").innerHTML+=nombre;
+}
+
 
 
 
